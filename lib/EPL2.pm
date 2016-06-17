e49442936fd2680bc5de137ed143c308c76b0abc
Katseres <tedkat@gmail.com>
2016-06-17 13:58:59 -0500

    fix doubles

--- lib/EPL2.pm
+++ lib/EPL2.pm
@@ -8,24 +8,6 @@ use 5.010;
 
 =pod
 
-=head1 NAME
-
-EPL2 - A Moosie way to Program EPL2 Printers
-
-=head1 VERSION
-
-version 0.001
-
-=head1 AUTHOR
-
-Ted Katseres <tedkat@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2011 by Ted Katseres.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
 
 =cut
 
